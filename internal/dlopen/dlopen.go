a1ad188b391c0ba4d780379952e9539912056504
Oleg Gaidarenko <markelog@gmail.com>
2021-01-27 03:43:23 +0300

    dlopen

--- internal/dlopen/dlopen.go
+++ internal/dlopen/dlopen.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build !windows
+
 // Package dlopen provides some convenience functions to dlopen a library and
 // get its symbols.
 package dlopen
